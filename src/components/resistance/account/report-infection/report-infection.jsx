2c0962034982de06b19599145940352c6518fa6f
Fábio Henrique <997525+fabiosjc@users.noreply.github.com>
2019-12-27 16:18:33 -0300

    production improvements

--- src/components/resistance/account/report-infection/report-infection.jsx
+++ src/components/resistance/account/report-infection/report-infection.jsx
@@ -5,7 +5,6 @@ import Dialog from '@material-ui/core/Dialog';
 import DialogActions from '@material-ui/core/DialogActions';
 import DialogContent from '@material-ui/core/DialogContent';
 import DialogTitle from '@material-ui/core/DialogTitle';
-import { Divider } from '@material-ui/core';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faBiohazard } from '@fortawesome/free-solid-svg-icons';
 import { NotificationManager } from 'react-notifications';
